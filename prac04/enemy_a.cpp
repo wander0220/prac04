152fd2e27d6721eaf5b130b7286b904a55494ce4
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-06-16 19:27:29 +0900

    enemy가 찌끄러기로 나온 이유 64*64 인데 16*16으로 착각함

--- prac04/enemy_a.cpp
+++ prac04/enemy_a.cpp
@@ -14,16 +14,16 @@ void EnemyA::Update()
 }
 void EnemyA::Render() 
 {
-    TextureElement* enemy = textureManager.GetTexture(GAME_PLAYER_BODY);
+    TextureElement* enemy = textureManager.GetTexture(GAME_ENEMY_BODY);
     enemy->Sprite->Begin(D3DXSPRITE_ALPHABLEND);
 
     RECT srcRect;
     srcRect.left = 0;
     srcRect.top = 0;
-    srcRect.right = 16;
-    srcRect.bottom = 16;
+    srcRect.right = 64;
+    srcRect.bottom = 64;
 
-    D3DXVECTOR3 pos(posX - 8, posY - 8, 0);
+    D3DXVECTOR3 pos(posX - 32, posY - 32, 0);
    
     enemy->Sprite->Draw(enemy->Texture, &srcRect, nullptr, &pos
         , D3DCOLOR_XRGB(255, 255, 255));
